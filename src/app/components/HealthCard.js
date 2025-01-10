8d5f371851edf63634cb46702b07de71065cf952
KingsukNandi <kingsuk24nandi@gmail.com>
2025-01-10 10:36:42 +0530

    fix

--- src/app/components/HealthCard.js
+++ src/app/components/HealthCard.js
@@ -1,7 +1,9 @@
 import { Icon } from "@iconify/react";
 import EcgData from "./EcgData";
+import { useRouter } from "next/navigation";
 
 const HealthCard = () => {
+  const router = useRouter();
     return (
         <div className="bg-[#d0e9bc] flex p-2 rounded-3xl">
           <div className="w-1/2 p-3">
@@ -14,8 +16,8 @@ const HealthCard = () => {
               />
             </div>
             <p className="text-2xl font-semibold py-3">Health</p>
-            <p className="text-sm pb-3">Check your heart&apos;s health</p>
-            <button className="font-semibold border border-black rounded-full px-4 py-1">
+            <p className="text-sm pb-3">Check your mental health</p>
+            <button onClick={() => router.push('/diagnosis')} className="font-semibold border border-black rounded-full px-4 py-1">
               Diagnostic
             </button>
           </div>
